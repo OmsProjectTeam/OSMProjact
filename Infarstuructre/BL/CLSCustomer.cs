e8e9586c28efbe012631192c2d45bfd4312318f6
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2024-06-20 18:21:38 +0600

    add customer api and its bl and entity

--- Infarstuructre/BL/CLSCustomer.cs
+++ Infarstuructre/BL/CLSCustomer.cs
@@ -1,4 +1,8 @@
 ﻿
+using Domin.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
 namespace Infarstuructre.BL
 {
     public interface IICustomer
@@ -9,7 +13,14 @@ namespace Infarstuructre.BL
         bool UpdateData(Customer updatss);
         bool deleteData(int id);
         List<TBViewCustomers> GetAllv(int id);
-    }
+        Task<IEnumerable<TBViewCustomers>>? GetAllCustomersAsync();
+		Task<IEnumerable<TBViewCustomers>> GetAllCustomersWithConditionAsync(Expression<Func<TBViewCustomers, bool>> condition);
+		Task<Customer>? GetCustomerAsync(int id);
+		Task AddCustomerAsync(Customer customer);
+		Task UpdateCustomerAsync(Customer customer);
+
+
+	}
     public class CLSCustomer: IICustomer
     {
         MasterDbcontext dbcontext;
@@ -77,5 +88,35 @@ namespace Infarstuructre.BL
             return MySlider;
         }
 
-    }
+		public async Task<IEnumerable<TBViewCustomers>>? GetAllCustomersAsync()
+		{
+			IEnumerable<TBViewCustomers> customers = await dbcontext.ViewCustomers.OrderByDescending(n => n.id).Where(a => a.CurrentState == true).ToListAsync();
+			return customers;
+		}
+
+		public async Task<IEnumerable<TBViewCustomers>> GetAllCustomersWithConditionAsync(Expression<Func<TBViewCustomers, bool>> condition)
+		{
+			IEnumerable<TBViewCustomers> customers = await dbcontext.ViewCustomers.Where(condition).ToListAsync();
+			return customers;
+		}
+
+		public async Task<Customer>? GetCustomerAsync(int id)
+		{
+			Customer customer = await dbcontext.customers.FindAsync(id);
+			return customer;
+		}
+
+		public async Task AddCustomerAsync(Customer customer)
+		{
+			await dbcontext.AddAsync(customer);
+			await dbcontext.SaveChangesAsync();
+		}
+
+		public async Task UpdateCustomerAsync(Customer customer)
+		{
+			dbcontext.Entry(customer).State = EntityState.Modified;
+			await dbcontext.SaveChangesAsync();
+		}
+	}
+
 }
---
+++ Yara/Areas/Admin/APIsControllers/CustomerAPIController.cs
@@ -0,0 +1,178 @@
+﻿using Domin.Entity;
+using System.Linq.Expressions;
+using System.Net;
+
+namespace Yara.Areas.Admin.API_Controller;
+
+[Authorize(Roles = "Admin,ApiRoles")]
+[Route("api/[controller]")]
+[ApiController]
+public class CustomerAPIController : ControllerBase
+{
+	private ApiResponse _response;
+	public CustomerAPIController(IICustomer iCustomer)
+    {
+        this.iCustomer = iCustomer;
+        _response = new ApiResponse();
+    }
+
+    private readonly IICustomer iCustomer;
+
+    [HttpGet("get")]
+    public async Task<ActionResult<IEnumerable<TBViewCustomers>>> Get()
+	{
+        try
+        {
+            var customers = await iCustomer.GetAllCustomersAsync();
+            if (customers == null)
+                _response.StatusCode = HttpStatusCode.BadRequest;
+
+            _response.Result = customers;
+            _response.StatusCode = HttpStatusCode.OK;
+
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        { 
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string> { ex.Message };
+        }
+        return Ok(_response);
+	}
+
+    [HttpPost("GitAllCustomers")]
+    public async Task<ActionResult<IEnumerable<TBViewCustomers>>> GitAllCustomers()
+    {
+        try
+        {
+			var customers = await iCustomer.GetAllCustomersAsync();
+			if (customers == null)
+				_response.StatusCode = HttpStatusCode.BadRequest;
+
+			_response.Result = customers;
+			_response.StatusCode = HttpStatusCode.Created;
+
+			return Ok(_response);
+		}
+        catch (Exception ex)
+		{
+			_response.IsSuccess = false;
+			_response.ErrorMessage = new List<string> { ex.Message };
+		}
+        return Ok(_response);
+    }
+
+    [HttpPost("GitAllCustomersWithCondition")]
+    public async Task<ActionResult<IEnumerable<TBViewCustomers>>> GitAllCustomersWithCondition(Expression<Func<TBViewCustomers, bool>> condition)
+    {
+        try
+        {
+            var customers = await iCustomer.GetAllCustomersWithConditionAsync(condition);
+            if (customers == null)
+                _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.Result = customers;
+            _response.StatusCode = HttpStatusCode.Created;
+
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string> { ex.Message };
+        } 
+        return Ok(_response);
+    }
+
+    [HttpPost("{id}")]
+    public async Task<ActionResult<TBViewCustomers>> GitCustomerById(int id)
+    {
+        try
+        {
+            var customer = await iCustomer.GetCustomerAsync(id);
+            if (customer == null)
+                _response.StatusCode = HttpStatusCode.BadRequest;
+            _response.Result = customer;
+            _response.StatusCode = HttpStatusCode.Created;
+
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string> { ex.Message };
+        }
+
+        return Ok(_response);
+	}
+
+    [HttpPost]
+    public async Task<ActionResult> AddCustomer(Customer customer)
+    {
+        try
+        {
+            if (!ModelState.IsValid)
+                _response.StatusCode = HttpStatusCode.BadRequest;
+
+            await iCustomer.AddCustomerAsync(customer);
+
+            _response.Result = customer;
+            return Ok(_response);
+        }
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string> { ex.Message };
+        }
+
+        return Ok(_response);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult> EditCustomer(int id, [FromBody] Customer customer)
+    {
+        try
+        {
+			var custom = await iCustomer.GetCustomerAsync(id);
+			if (custom == null)
+				_response.StatusCode = HttpStatusCode.BadRequest;
+
+			custom = customer;
+			await iCustomer.UpdateCustomerAsync(custom);
+
+			_response.Result = custom;
+			return Ok(_response);
+		}
+        catch (Exception ex)
+        {
+            _response.IsSuccess = false;
+            _response.ErrorMessage = new List<string> { ex.Message };
+        }
+
+        return Ok(_response);
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<ActionResult> DeleteCustomer(int id)
+    {
+        try
+        {
+			var custom = await iCustomer.GetCustomerAsync(id);
+			if (custom == null)
+				_response.StatusCode = HttpStatusCode.BadRequest;
+
+			custom.CurrentState = false;
+			await iCustomer.UpdateCustomerAsync(custom);
+
+			_response.Result = custom;
+			return Ok(_response);
+		}
+        catch (Exception ex)
+		{
+			_response.IsSuccess = false;
+			_response.ErrorMessage = new List<string> { ex.Message };
+		}
+
+		return Ok(_response);
+    }
+}
+
