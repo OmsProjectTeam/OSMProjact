4b2ab7bf343fec0c01e8d3c41ea6d903625f146d
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2024-06-20 15:57:04 +0600

    add apiResponse class for managing api state and result

---
+++ Infarstuructre/ViewModel/ApiResponse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infarstuructre.ViewModel
+{
+	public class ApiResponse
+	{
+		public ApiResponse()
+		{
+			ErrorMessage = new List<string>();
+		}
+
+		public HttpStatusCode StatusCode { get; set; }
+		public bool IsSuccess { get; set; } = true;
+		public List<string> ErrorMessage { get; set; }
+		public object Result { get; set; }
+	}
+}
