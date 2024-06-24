ee61f4ff93c02cb453847c8fbbfaea670b85f585
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2024-06-25 03:12:12 +0600

    update merchant profile controller

--- Yara/Areas/merchantAccount/Controllers/ProfileController.cs
+++ Yara/Areas/merchantAccount/Controllers/ProfileController.cs
@@ -12,24 +12,24 @@ namespace Yara.Areas.merchantAccount.Controllers
             iMerchant = iMerchant1;
 			_userManager = userManager;
         }
-        public async Task<IActionResult> MyProfile()
+        public async Task<IActionResult> MyProfile(string id)
         {
             // Get the current logged-in user
-            var user = await _userManager.GetUserAsync(User);
+            var user = await _userManager.GetUserIdAsync(string id);
             if (user == null)
             {
                 return Unauthorized();
             }
-            if (!int.TryParse(user.Id, out int merchantId))
-            {
-                return BadRequest("Invalid merchant ID.");
-            }
+            //if (!int.TryParse(user.Id, out int merchantId))
+            //{
+            //    return BadRequest("Invalid merchant ID.");
+            //}
 
-            var merchant = await iMerchant.GetMerchantAsync(merchantId);
-            if (merchant == null)
-            {
-                return NotFound();
-            }
+            //var merchant = await iMerchant.GetMerchantAsync(merchantId);
+            //if (merchant == null)
+            //{
+            //    return NotFound();
+            //}
 
             var viewModel = new ViewmMODeElMASTER
             {
