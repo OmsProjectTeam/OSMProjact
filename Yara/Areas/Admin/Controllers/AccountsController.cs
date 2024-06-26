b5aa240e01c48aa32aa4524a158c3cdf8cf571dd
Alifouaad24 <161458284+Alifouaad24@users.noreply.github.com>
2024-06-25 22:59:17 -0700

    Ali Update AirFirth Area

--- Yara/Areas/Admin/Controllers/AccountsController.cs
+++ Yara/Areas/Admin/Controllers/AccountsController.cs
@@ -282,8 +282,12 @@ namespace Yara.Areas.Admin.Controllers
 				{
 					if (roles.Contains("Customer"))
 					{
-						// Redirect to merchant area with user ID
 						return RedirectToAction("Index", "Home", new { area = "ClintAccount", userId = user.Id });
+					}
+					if (roles.Contains("AirFreight"))
+					{
+
+						return RedirectToAction("Index", "Home", new { area = "AirFreight", userId = user.Id });
 					}
 					return RedirectToAction(nameof(Registers));
 				}
@@ -800,8 +804,11 @@ namespace Yara.Areas.Admin.Controllers
 				{
 					if (roles.Contains("Customer"))
 					{
-						// Redirect to merchant area with user ID
 						return RedirectToAction("Index", "Home", new { area = "ClintAccount", userId = user.Id});
+					}
+					if (roles.Contains("AirFreight"))
+					{
+						return RedirectToAction("Index", "Home", new { area = "AirFreight", userId = user.Id });
 					}
 					return RedirectToAction("Registers");
 					//var oldRole = await _userManager.GetRolesAsync(userUpdate);
