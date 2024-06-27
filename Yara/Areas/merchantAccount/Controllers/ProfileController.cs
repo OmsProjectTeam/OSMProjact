a3d396d750bac498d26922acc4f18770ca282d45
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2024-06-27 17:24:54 +0600

    update profile for merchant

--- Yara/Areas/merchantAccount/Controllers/ProfileController.cs
+++ Yara/Areas/merchantAccount/Controllers/ProfileController.cs
@@ -3,64 +3,65 @@ using Infarstuructre.BL;
 
 namespace Yara.Areas.merchantAccount.Controllers
 {
-	[Area("merchantAccount")]
-	[Authorize(Roles = "Admin,Merchant")]
-	public class ProfileController : Controller
-	{
-		private readonly UserManager<ApplicationUser> _userManager;
-		private readonly IIUserInformation iUserInformation;
-		private readonly IHttpClientFactory _httpClient;
+    [Area("merchantAccount")]
+    [Authorize(Roles = "Admin,Merchant")]
+    public class ProfileController : Controller
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IIUserInformation iUserInformation;
+        private readonly IHttpClientFactory _httpClient;
 
 
-		public ProfileController(UserManager<ApplicationUser> userManager, IHttpClientFactory httpClient, IIUserInformation iUserInformation)
-		{
-			_userManager = userManager;
-			_httpClient = httpClient;
-			this.iUserInformation = iUserInformation;
-		}
+        public ProfileController(UserManager<ApplicationUser> userManager, IHttpClientFactory httpClient, IIUserInformation iUserInformation)
+        {
+            _userManager = userManager;
+            _httpClient = httpClient;
+            this.iUserInformation = iUserInformation;
+        }
 
+        public async Task<IActionResult> MyProfile(string userId)
+        {
 
-		public async Task<IActionResult> MyProfile(string id)
-		{
-			var user = await _userManager.FindByIdAsync(id);
-			if (user == null)
-				return NotFound();
+            ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
+            var userd = vmodel.sUser = iUserInformation.GetById(userId);
 
-			return View(user);
-		}
+            var user = await _userManager.FindByIdAsync(userId);
+            if (user == null)
+                return NotFound();
 
-		public async Task<IActionResult> ShowUserData(string id)
-		{
-			ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
-			//vmodel.ListVwUser = iUserInformation.GetAll();
-			if (id != null)
-			{
-				vmodel.sUser = iUserInformation.GetById(Convert.ToString(id));
-				return View(vmodel);
-			}
-			else
-			{
-				return View(new RegisterViewModel());
-			}
-		}
+            return View(vmodel);
+        }
 
-		public IActionResult ChangePassword(string Id)
-		{
-			ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
-			//vmodel.ListVwUser = iUserInformation.GetAll();
-			if (Id != null)
-			{
-				vmodel.sUser = iUserInformation.GetById(Convert.ToString(Id));
-				return View(vmodel);
-			}
-			else
-			{
-				return View(new RegisterViewModel());
-			}
-		}
-	}
+        public async Task<IActionResult> ShowUserData(string id)
+        {
+            ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
+            //vmodel.ListVwUser = iUserInformation.GetAll();
+            if (id != null)
+            {
+                vmodel.sUser = iUserInformation.GetById(Convert.ToString(id));
+                return View(vmodel);
+            }
+            else
+            {
+                return View(new RegisterViewModel());
+            }
+        }
 
+        public IActionResult ChangePassword(string Id)
+        {
+            ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
+            //vmodel.ListVwUser = iUserInformation.GetAll();
+            if (Id != null)
+            {
+                vmodel.sUser = iUserInformation.GetById(Convert.ToString(Id));
+                return View(vmodel);
+            }
+            else
+            {
+                return View(new RegisterViewModel());
+            }
+        }
 
-      
     }
+}
 
