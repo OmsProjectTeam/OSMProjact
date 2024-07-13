24b7f9cdbc150830dc703967c99357947b69f8cb
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2024-07-13 18:05:31 +0600

    update client index and home controller for displaying order informatioin at homepage

--- Yara/Areas/ClintAccount/Controllers/HomeController.cs
+++ Yara/Areas/ClintAccount/Controllers/HomeController.cs
@@ -11,25 +11,64 @@ public class HomeController : Controller
 {
     private readonly UserManager<ApplicationUser> _userManager;
 	IIUserInformation iUserInformation;
+    private readonly IIOrder iOrder;
+    private readonly IIOrderNew iOrderNew;
 
-	public HomeController(UserManager<ApplicationUser> userManager, IIUser iUser,IIUserInformation iUserInformation1)
+    public HomeController(UserManager<ApplicationUser> userManager, IIUser iUser,IIUserInformation iUserInformation1, IIOrder iOrder, IIOrderNew iOrderNew)
 	{
 		_userManager = userManager;
 		iUserInformation= iUserInformation1;
-	}
+        this.iOrder = iOrder;
+        this.iOrderNew = iOrderNew;
+    }
 	public async Task<IActionResult> Index(string userId)
 	{
+        ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
+        var userd = vmodel.sUser = iUserInformation.GetById(userId);
 
+        var user = await _userManager.FindByIdAsync(userId);
+        //var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+            return NotFound();
 
-		
-		return View();
-	}
+        string phoneNumber = user.PhoneNumber;
+        if (string.IsNullOrEmpty(phoneNumber))
+        {
+            return View();
+        }
+        var newOrders = await iOrderNew.GetOrdersByPhoneAsync(phoneNumber);
+        var oldOrders = await iOrder.GetOrdersByPhoneAsync(phoneNumber);
+
+        ViewBag.NewOrders = newOrders;
+        ViewBag.OldOrders = oldOrders;
+        ViewBag.PhoneNumber = phoneNumber;
+
+        return View(vmodel);
+    }
 
 	public async Task<IActionResult> IndexAr(string userId)
 	{
 
+        ViewmMODeElMASTER vmodel = new ViewmMODeElMASTER();
+        var userd = vmodel.sUser = iUserInformation.GetById(userId);
 
+        var user = await _userManager.FindByIdAsync(userId);
+        //var user = await _userManager.GetUserAsync(User);
+        if (user == null)
+            return NotFound();
 
-		return View();
-	}
+        string phoneNumber = user.PhoneNumber;
+        if (string.IsNullOrEmpty(phoneNumber))
+        {
+            return View();
+        }
+        var newOrders = await iOrderNew.GetOrdersByPhoneAsync(phoneNumber);
+        var oldOrders = await iOrder.GetOrdersByPhoneAsync(phoneNumber);
+
+        ViewBag.NewOrders = newOrders;
+        ViewBag.OldOrders = oldOrders;
+        ViewBag.PhoneNumber = phoneNumber;
+
+        return View(vmodel);
+    }
 }
