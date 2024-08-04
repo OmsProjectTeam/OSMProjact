aa4c3927b016c4dcb5645ac1073eec320bf547d3
Alifouaad24 <161458284+Alifouaad24@users.noreply.github.com>
2024-08-03 17:30:37 -0700

    Theard push

--- Yara/Areas/Admin/Controllers/ChatController.cs
+++ Yara/Areas/Admin/Controllers/ChatController.cs
@@ -5,15 +5,15 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Yara.Areas.Admin.Controllers
 {
-	[Area("Admin")]
-	[Authorize(Roles = "Admin")]
-	public class ChatController : Controller
-	{
+    [Area("Admin")]
+    [Authorize(Roles = "Admin")]
+    public class ChatController : Controller
+    {
         MasterDbcontext db;
-		IIConnectAndDisconnect iConnectAndDisconnect;
-		IIUserInformation iUserInformation;
-		IIMessageChat iMessageChat;
-		UserManager<ApplicationUser> iUserManager;
+        IIConnectAndDisconnect iConnectAndDisconnect;
+        IIUserInformation iUserInformation;
+        IIMessageChat iMessageChat;
+        UserManager<ApplicationUser> iUserManager;
         public ChatController(IIConnectAndDisconnect iConnectAndDisconnect1, IIMessageChat iMessageChat1, IIUserInformation iUserInformation1, UserManager<ApplicationUser> iUserManager1, MasterDbcontext db)
         {
             iConnectAndDisconnect = iConnectAndDisconnect1;
@@ -23,83 +23,46 @@ namespace Yara.Areas.Admin.Controllers
             this.db = db;
         }
         public async Task<IActionResult> Index()
-		{
+        {
             ViewmMODeElMASTER viewmMODeElMASTER = new ViewmMODeElMASTER();
             var currentUserId = iUserManager.GetUserId(User);
 
-			viewmMODeElMASTER.ViewChatMessage = iMessageChat.GetByReciverId(currentUserId);
-			
+            viewmMODeElMASTER.ViewChatMessage = iMessageChat.GetByReciverId(currentUserId);
+
 
             return View(viewmMODeElMASTER);
-		}
+        }
 
         [AllowAnonymous]
         [HttpGet("OwnChat/{anotherId}")]
         [Route("/Admin/Chat/OwnChat/{anotherId}")]
-        public async Task<IActionResult> OwnChat(string anotherId) 
-		{
-            var viewModel = new ViewmMODeElMASTER();
+        public async Task<IActionResult> OwnChat(string anotherId)
+        {
+            ViewmMODeElMASTER viewmMODeElMASTER = new ViewmMODeElMASTER();
+            var users = viewmMODeElMASTER.ConnectAndDisConnect = iConnectAndDisconnect.GetAll();
+
             var currentUserId = iUserManager.GetUserId(User);
             ViewBag.MyId1 = currentUserId;
 
             var IamSender = iMessageChat.GetBySenderIdAndReciverId(currentUserId, anotherId);
             var IamReciver = iMessageChat.GetBySenderIdAndReciverId(anotherId, currentUserId);
-            IamSender.AddRange(IamReciver);
+            ///////////////////////////////////////////////////////////////////////////////////////////
 
-            viewModel.ViewChatMessage = IamSender.OrderBy(m => m.MessageeTime).ToList();
-            ViewBag.another = iUserInformation.GetById(anotherId).UserName;
-            ViewBag.anotherId = anotherId;
-            ViewBag.img = iUserInformation.GetById(currentUserId).ImageUser;
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            foreach (var item in IamReciver)
+            {
+                IamSender.Add(item);
+            }
+
+            viewmMODeElMASTER.ViewChatMessage = IamSender;
+            ViewBag.another = (iUserInformation.GetById(anotherId)).UserName;
+            ViewBag.anotherId = (iUserInformation.GetById(anotherId)).Id;
+            //viewmMODeElMASTER.ViewChatMessage = iMessageChat.GetByReciverId(currentUserId);
+            ViewBag.img = (iUserInformation.GetById(currentUserId)).ImageUser;
             ViewBag.UserId = currentUserId;
 
-            return View(viewModel);
+            return View(viewmMODeElMASTER);
         }
-		//      [HttpGet]
-		//      [Route("/Admin/Chat/OwnChat/{anotherId}")]
-		//      public async Task<IActionResult> OwnChat(string anotherId) 
-		//{
-		//          ViewmMODeElMASTER viewmMODeElMASTER = new ViewmMODeElMASTER();
-		//          var users = viewmMODeElMASTER.ConnectAndDisConnect = iConnectAndDisconnect.GetAll();
-
-		//          var currentUserId = iUserManager.GetUserId(User);
-
-		//          var IamSender = iMessageChat.GetBySenderIdAndReciverId(currentUserId, anotherId);
-		//          var IamReciver = iMessageChat.GetBySenderIdAndReciverId(anotherId, currentUserId);
-		//          ///////////////////////////////////////////////////////////////////////////////////////////
-
-		//          /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-		//          foreach (var item in IamReciver)
-		//          {
-		//              IamSender.Add(item);
-		//          }
-
-		//          viewmMODeElMASTER.ViewChatMessage = IamSender;
-		//          ViewBag.another = (iUserInformation.GetById(anotherId)).UserName;
-		//          ViewBag.anotherId = (iUserInformation.GetById(anotherId)).Id;
-		//          //viewmMODeElMASTER.ViewChatMessage = iMessageChat.GetByReciverId(currentUserId);
-		//          ViewBag.img = (iUserInformation.GetById(currentUserId)).ImageUser;
-		//          ViewBag.UserId = currentUserId;
-
-		//          return View(viewmMODeElMASTER);
-		//      }
-
-		// =============================================================================
-
-		//[HttpPost]
-		//      [Route("/Admin/chat/uploadFile")]
-		//      public async Task<IActionResult> UploadFile(IFormFile file)
-		//      {
-		//          if (file == null || file.Length == 0)
-		//              return Ok("null");
-
-		//          string fileName = Guid.NewGuid().ToString();
-		//          var filePath = Path.Combine("wwwroot/Images/Home/", fileName + file.FileName);
-
-		//          using (var stream = new FileStream(filePath, FileMode.Create))
-		//          {
-		//              await file.CopyToAsync(stream);
-		//          }
-
 
         [HttpGet("Refresh/{anotherId}")]
         [Route("/Admin/Chat/Refresh/{anotherId}")]
@@ -131,38 +94,15 @@ namespace Yara.Areas.Admin.Controllers
             if (file == null || file.Length == 0)
                 return Ok("null");
 
-			string fileName = Guid.NewGuid().ToString();
-			var filePath = Path.Combine("wwwroot/Images/Home/", fileName + file.FileName);
+            string fileName = Guid.NewGuid().ToString();
+            var filePath = Path.Combine("wwwroot/Images/Home/", fileName + file.FileName);
 
-			using (var stream = new FileStream(filePath, FileMode.Create))
-			{
-				await file.CopyToAsync(stream);
-			}
-
-			return Ok(new { filePath = $"/Images/Home/{file.FileName}" });
-		}
-
-        [HttpGet]
-        [Route("/Admin/Chat/GetMessages")]
-        public async Task<IActionResult> GetMessages(string anotherId)
-        {
-            var currentUserId = iUserManager.GetUserId(User);
-
-            var IamSender = iMessageChat.GetBySenderIdAndReciverId(currentUserId, anotherId);
-            var IamReciver = iMessageChat.GetBySenderIdAndReciverId(anotherId, currentUserId);
-            IamSender.AddRange(IamReciver);
-
-            var messages = IamSender.OrderBy(m => m.MessageeTime).Select(m => new
+            using (var stream = new FileStream(filePath, FileMode.Create))
             {
-                m.Message,
-                m.SenderId,
-                m.ReciverId,
-                m.ImgMsg,
-                m.MessageeTime
-            }).ToList();
+                await file.CopyToAsync(stream);
+            }
 
-            return Json(messages);
+            return Ok(new { filePath = $"/Images/Home/{file.FileName}" });
         }
     }
 }
-
