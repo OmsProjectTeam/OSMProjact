3bd21a1734dafb1c3fc00ba667f78e80e7b51487
Masum Rayhan <73586966+masum-rayhan@users.noreply.github.com>
2024-09-09 04:02:40 +0600

    update paidings

---
+++ Yara/wwwroot/js/paidings.js
@@ -0,0 +1,49 @@
+﻿$(document).ready(function () {
+    $('#orderId').change(function () {
+        updateCosting(false);
+    });
+
+    $('#toCurrency').change(function () {
+        updateCosting(false);
+    });
+
+    $('#revisedMoney').on('input', function () {
+        updateCosting(true);
+    });
+
+    function updateCosting(isManual) {
+        const orderId = $('#orderId').val();
+        const revisedMoney = $('#revisedMoney').val();
+        const toCurrency = $('#toCurrency').val();
+
+        $.ajax({
+            url: getOrderDetailsUrl,
+            data: {
+                toCurrencyId: toCurrency,
+                fromCurrencyId: 1,
+                revisedMoney: revisedMoney,
+                orderId: orderId,
+                isManual: isManual
+            },
+            success: function (data) {
+                if (data) {
+                    $('#exchangePrice').val(data.exchangedPrice);
+                    if (!isManual) {
+                        $('#revisedMoney').val(data.revisedMoney);
+                    }
+                } else {
+                    $('#exchangePrice').val('');
+                    $('#revisedMoney').val('');
+                }
+            },
+            error: function () {
+                alert('Error! Please try again.');
+            }
+        });
+    }
+
+    var loadFile = function (event) {
+        var image = document.getElementById('output');
+        image.src = URL.createObjectURL(event.target.files[0]);
+    };
+});
